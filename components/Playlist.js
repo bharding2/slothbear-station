342887daa688902c589e34e4aa6f4dd062af65a2
bharding2 <b.harding@gmail.com>
2018-10-28 23:41:50 -0700

    swapping mapped songs for single form

--- components/Playlist.js
+++ components/Playlist.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PlaylistSong from './PlaylistSong';
+import PlaylistUpdateForm from './PlaylistUpdateForm';
 import './Playlist.css';
 
 class Playlist extends Component {
@@ -10,20 +10,11 @@ class Playlist extends Component {
   render() {
     let playlistEmbedSrc = `https://www.youtube.com/embed/?listType=playlist&list=${this.props.playlistId}&showinfo=1`;
 
-    let playlistItems = this.props.playlist.map((playlistItem) => {
-      let videoId = playlistItem.contentDetails.videoId;
-      let songTitle = playlistItem.snippet.title.split(' (')[0];
-
-      return(
-        <PlaylistSong key={ videoId } songTitle={ songTitle } />
-      )
-    });
-
     return (
       <div>
-        <iframe width="560" height="315" src={ playlistEmbedSrc } frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        <iframe width="560" height="315" src={ playlistEmbedSrc } frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>\
 
-        { playlistItems }
+        <PlaylistUpdateForm playlist={ this.props.playlist } pandora={ this.props.pandora } />
       </div>
     )
   }

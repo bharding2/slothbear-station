ea4a4a838c0bd28044ce9b2c1f6d5473b4d4f4e8
bharding2 <b.harding@gmail.com>
2018-11-07 01:38:52 -0800

    scaffold for pandora song selection

--- components/PlaylistUpdateForm.js
+++ components/PlaylistUpdateForm.js
@@ -10,28 +10,40 @@ class PlaylistUpdateForm extends Component {
     let playlistItems = this.props.playlist.map((playlistItem, idx) => {
       let videoId = playlistItem.contentDetails.videoId;
       let songTitle = playlistItem.snippet.title.split(' (')[0];
+      let pandoraSelectOptions = [];
 
+      if (playlistItem.pandoraSelectOptions) {
+        pandoraSelectOptions = playlistItem.pandoraSelectOptions.map((selectOption) => {
+          return (
+            <option value={ selectOption.musicToken }>{ `${selectOption.artistName} - ${selectOption.songName}` }</option>
+          )
+        })
+      }
+      
       return (
         <form id={ idx } onSubmit={ this.props.handleSubmit }>
           <p key={ videoId }>{ songTitle }</p>
           <p>
-            <label htmlFor="pandoraSongId">pandora song id</label>
-            <input name="pandoraSongId" type="text" readOnly value={ playlistItem.songId }/>
+            <label htmlFor="pandoraMusicToken">pandora music token</label>
+            <input name="pandoraMusicToken" type="text" readOnly value={ playlistItem.pandoraMusicToken }/>
           </p>
           <p>
             <label htmlFor="pandoraSongTitle">pandora song title</label>
-            <input name="pandoraSongTitle" type="text"/>
+            <input name="pandoraSongTitle" type="text" placeholder={ songTitle }/>
           </p>
+          <p>
+            <label htmlFor="pandoraSelect">pandora song select</label>
+          { playlistItem.pandoraSelectOptions &&
+            <select id="pandoraSelect">
+              { pandoraSelectOptions }
+            </select>
+          }
+          </p>
+          <input type="button" onClick={ this.props.updatePandoraSelectItems } />
           <input type="submit" value="submit change"/>
         </form>
       )
     });
-
-    // form
-    // playlist item - songTitle input to music.search for songs.musicToken
-    // search box (dropdown?)
-    // submit with musicToken, pandora songTitle, visual indication it has music token
-    // updates entire playlist
 
     return (
       <div>{ playlistItems }</div>
